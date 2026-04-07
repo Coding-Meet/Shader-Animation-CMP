3b85082acacaa587cd7ef45e44d090d30e60dced
meet <codingmeet26@gmail.com>
2026-04-07 19:16:28 +0530

    refactor: remove boilerplate KMP platform greeting files
    - Remove platform interface and expected function definitions
    - Delete Android and iOS platform implementations
    - Remove Greeting class and associated logic

--- composeApp/src/commonMain/kotlin/com/meet/shader/animation/cmp/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.meet.shader.animation.cmp
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/commonMain/kotlin/com/meet/shader/animation/cmp/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.meet.shader.animation.cmp
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
